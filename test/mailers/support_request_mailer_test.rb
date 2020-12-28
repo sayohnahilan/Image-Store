667ef8b863ab037fd53811fcd50cec2d3083f93f
sayohnahilan <sayohnahilan@gmail.com>
2020-12-27 19:09:33 -0500

    origin for this test

--- test/mailers/support_request_mailer_test.rb
+++ test/mailers/support_request_mailer_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SupportRequestMailerTest < ActionMailer::TestCase
   test "respond" do
-    mail = SupportRequestMailer.respond(:one)
+    mail = SupportRequestMailer.respond
     assert_equal "Respond", mail.subject
     assert_equal ["to@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
