2bf5b006f73ab06a01c6a8bcc953b11596e61de6
sayohnahilan <sayohnahilan@gmail.com>
2020-12-28 21:00:21 -0500

    cleaning up code

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
-  skip_before_action :authorize, only: :create
   include CurrentCart
+
+  skip_before_action :authorize, only: :create
   before_action :set_cart, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
